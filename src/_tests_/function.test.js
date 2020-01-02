0172bee9e7b4ff1cf6895e7cc92d6bb589582b58
John Lewis <johnlewis@MacBook-Pro.local>
2020-01-02 11:57:14 -0700

    more tests

--- src/_tests_/function.test.js
+++ src/_tests_/function.test.js
@@ -1,42 +1,50 @@
-import {returnText} from '../functions/returnText'
-import React from 'react'
+import { returnText } from "../functions/returnText";
+import React from "react";
+// import { act } from "react-dom/test-utils";
+// import { render } from "@testing-library/react";
 
 const play = {
-    "penalty": "",
-    "yards": "",
-    "penTeam": "",
-    "playType": "Pass",
-    "gainLoss": "loss",
-    "playDist": "1",
-    "player1": "S. Ehlinger",
-    "player2": "B. Eagles",
-    "kicker": "",
-    "result": "4th",
-    "afterTD": "",
-    "min": "1",
-    "sec": "7",
-    "quarter": "first",
-    "kickType": "",
-    "playCount": {
-        "$numberInt": "7"
-    }
-}
+  penalty: "",
+  yards: "",
+  penTeam: "",
+  playType: "Pass",
+  gainLoss: "loss",
+  playDist: "1",
+  player1: "S. Ehlinger",
+  player2: "B. Eagles",
+  kicker: "",
+  result: "4th",
+  afterTD: "",
+  min: "1",
+  sec: "7",
+  quarter: "first",
+  kickType: "",
+  playCount: {
+    $numberInt: "7"
+  }
+};
 
+describe("returnText function tests", () => {
+  it("should return something", () => {
+    let result = returnText(play);
+    expect(result).toBeTruthy();
+  });
+  it("to contain last_name", () => {
+    let result = returnText(play);
+    expect(typeof result).toBe("object");
+  });
+  it("is p element", () => {
+    let result = returnText(play);
 
-describe('returnText function tests', ()=> {
-    it('should return something', ()=> {
+    expect(result).toMatchObject({ type: "p" });
+  });
+  it("contains", () => {
+    let result = returnText(play);
 
-        let result = returnText(play)
-        expect(result).toBeTruthy()
-    })
-    it('to contain last_name', ()=> {
-        let result = returnText(play)
-        expect(typeof result).toBe( 'object' )
-    })
-    
-})
-
-
-
-
-
+    expect.objectContaining(
+      <p>
+        Pass by S. Ehlinger to B. Eagles with a loss of 1 yards, resulting in a 4th.(01:07)
+      </p>
+    );
+  });
+});
